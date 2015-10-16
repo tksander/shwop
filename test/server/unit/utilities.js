71c895032df63c5298b67d5253c50a5e75c3cfd7
Brett Sanders <bsand08@gmail.com>
2015-10-16 15:26:17 -0700

    (test) Create test folder structure

---
+++ test/server/unit/utilities.js
@@ -0,0 +1,13 @@
+var assert = chai.assert;
+var should = chai.should();
+var expect = chai.expect;
+
+describe('the todo.App', function() {
+  describe('the todo object', function(){
+
+    it('should be an object', function(){
+      expect(1).to.equal(1);
+    });
+
+  });
+});
