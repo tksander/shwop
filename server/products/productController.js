4065d2672a5ead22d549b858e382426cf67e1db7
Brett Sanders <bsand08@gmail.com>
2015-10-16 14:52:41 -0700

    (feat) Update routers and middleware

---
+++ server/products/productController.js
@@ -0,0 +1,60 @@
+var Product = require('./productModel.js');
+var Q = require('q');  // a library for promises
+var util = require('../config/utils.js');
+
+
+module.exports = {
+
+  // retrieve all the products from the database
+  allProducts: function (req, res, next) {
+
+
+  // var findAll = Q.nbind(Product.find, Product);
+
+  // findAll({})
+  //   .then(function (products) {
+  //     res.json(product);
+  //   })
+  //   .fail(function (error) {
+  //     next(error);
+  //   });
+  },
+
+  // adds a new product to the database
+  newProduct: function (req, res, next) {
+
+    
+    // var url = req.body.url;
+    // var createLink = Q.nbind(Link.create, Link);
+    // var findLink = Q.nbind(Link.findOne, Link);
+
+    // findLink({url: url})
+    //   .then(function (match) {
+    //     if (match) {
+    //       res.send(match);
+    //     } else {
+    //       return util.getUrlTitle(url);
+    //     }
+    //   })
+    //   .then(function (title) {
+    //     if (title) {
+    //       var newLink = {
+    //         url: url,
+    //         visits: 0,
+    //         base_url: req.headers.origin,
+    //         title: title
+    //       };
+    //       return createLink(newLink);
+    //     }
+    //   })
+    //   .then(function (createdLink) {
+    //     if (createdLink) {
+    //       res.json(createdLink);
+    //     }
+    //   })
+    //   .fail(function (error) {
+    //     next(error);
+    //   });
+  }
+
+};
