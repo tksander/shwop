bb854c8e75e8df79a86531e90b28a45dc6cf885b
Corey Roy <corey.j.roy@gmail.com>
2015-10-16 17:11:35 -0700

    (feat) Add sell.html & sell.js, to allow user to post new products for sale.

---
+++ client/app/sell/sell.js
@@ -0,0 +1,14 @@
+angular.module('shwop.sell', [])
+
+.controller('SellController', function ($http, $scope, $location, Products) {
+  $scope.product = {};
+  $scope.addProduct = function () {
+    Products.addProduct($scope.product)
+    .then(function (res) {
+      $location.path('/products');     // $location is Angular module. Keeps this a single-page
+    })                          // app & prevents controllers from being reloaded.
+    .catch(function (err) {
+      console.log(err);
+    });
+  };
+});
