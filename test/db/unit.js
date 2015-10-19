290bfce387bb8699b940ee4b86f021e6b398b49c
tksander <tkepasander@gmail.com>
2015-10-19 16:32:24 -0700

    Resolved merge conflict

--- test/db/unit.js
+++ test/db/unit.js
@@ -16,26 +16,27 @@ describe('Database', function () {
 
     });
 
-    // the before each section runs before each test
     beforeEach(function (done) {
+      console.log('Executing before protocol.');
 
-      // Disable the check for foreign keys to enable TRUCATE. Otherwise, we cannot clear b/c of constraints
-      db.Orm.query('SET FOREIGN_KEY_CHECKS = 0')
-      .then(function(){
-          return db.Orm.sync({ force: true });
-      })
-      .then(function(){
+    // Disable the check for foreign keys to enable TRUCATE. Otherwise, we cannot clear b/c of constraints
+    db.Orm.query('SET FOREIGN_KEY_CHECKS = 0')
+    .then(function(){
+        return db.Orm.sync({ force: true });
+    })
+    .then(function(){
 
-          return db.Orm.query('SET FOREIGN_KEY_CHECKS = 1')
-      })
-      .then(function(){
-          console.log('Database synchronised.');
-          done();
-      })
-      .catch(function(error) {
-        console.log('Found an error: ', error);
+        return db.Orm.query('SET FOREIGN_KEY_CHECKS = 1')
+    })
+    .then(function(){
+        console.log('Database synchronised.');
         done();
-      })
+    })
+    .catch(function(error) {
+      console.log('Found an error: ', error);
+      done();
+    })
+
     });
 
     describe('Create one record', function () {
