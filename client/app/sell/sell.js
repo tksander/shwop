8fca2b519e1824cc4b5866574990376e19b7c9d1
Corey Roy <corey.j.roy@gmail.com>
2015-10-20 17:02:23 -0700

    Add functionality to store tags as array, and draft functionality for uploading photos to Parse.

--- client/app/sell/sell.js
+++ client/app/sell/sell.js
@@ -1,7 +1,33 @@
 angular.module('shwop.sell', [])
 
-.controller('SellController', ['$http', '$scope', '$location', 'Products', function ($http, $scope, $location, Products) {
+.controller('SellController', ['$http', '$scope', '$location', 'Products', 'Photos', function ($http, $scope, $location, Products, Photos) {
   $scope.product = {};
+  $scope.product.tags = [];
+  $scope.product.photoURL = '';
+
+  var addToArray = function(item, array) {
+    if(array.indexOf(item) === -1 && !isBlank(item)){
+      array.push(item);
+      console.log('$scope.product.tags: ' + $scope.product.tags);
+    }
+  };
+
+  var isBlank = function(string) {
+    return string.trim() === '';
+  };
+
+  $scope.addTag = function() {
+    addToArray($scope.product.tag, $scope.product.tags);
+    $scope.product.tag = '';
+  };
+
+  $scope.addPhoto = function() {
+    $scope.filePath = '';
+    Photos.uploadPhoto($scope.productPhoto, function(url){
+      $scope.product.photoURL = url;
+    }.bind($scope));
+  };
+
   $scope.addProduct = function () {
     Products.addProduct($scope.product)
     .then(function (res) {
@@ -10,5 +36,21 @@ angular.module('shwop.sell', [])
     .catch(function (err) {
       console.log(err);
     });
+  };
+}])
+.directive('fileread', [function () {
+  return {
+      scope: {
+          fileread: '='
+      },
+      link: function (scope, element, attributes) {
+          element.bind('change', function (changeEvent) {
+              scope.$apply(function () {
+                  scope.fileread = changeEvent.target.files[0];
+                  // or all selected files:
+                  // scope.fileread = changeEvent.target.files;
+              });
+          });
+      }
   };
 }]);
