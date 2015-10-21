7089a6987cdbb387f4c5edd4c3a5aa85e70d63ca
tksander <tkepasander@gmail.com>
2015-10-21 16:56:42 -0700

    Created createProduct helper function

--- server/db/helpers.js
+++ server/db/helpers.js
@@ -17,9 +17,46 @@ var associateUserToProduct = function (userEmail, productName) {
   });
 };
 
-var createProduct = function (user, product, tags) {
-  
+var createUser = function (user) {
+
 };
+
+// User format is Object, Product format is Object, Tags format is Array
+var createProduct = function (user, product, tags) {
+  // FindOne with user.email
+  // findOrCreate tags
+  // Create product
+
+  // Allows for 
+  var productModel;
+  var userModel;
+
+  var promiseModels = [];
+  for(var i = 0; i < names.length; i++) {
+    promiseModels.push(db.Tag.findOrCreate({where: { tagName: promiseModels[i] }}));
+  }
+  promiseModels.push(db.User.findOne({where: {email: user.email}}));
+  promiseModels.push(db.Product.create(product));
+
+  Promise.all(promiseModels)
+  .spread(function () {
+    var args = Array.prototype.slice.call(arguments);
+    productModel = args.pop();
+    userModel = args.pop();
+
+    var results = [];
+    for(var i = 0; i < args.length; i++) {
+      results.push(args[i][0]);
+    }
+    // Save
+    return productModel.setTags(results)
+  })
+  .then(function (results) {
+    return productModel.setUser(userModel);
+  })
+};
+
+
 
 // var createProductAndAssociateToUser = function(userEmail, productName) {
 //   var promiseArray = [];
