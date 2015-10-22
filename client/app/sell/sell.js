b686fa1ea0bfd21dd6d932e4e759137bbfc4335c
tksander <tkepasander@gmail.com>
2015-10-22 13:20:26 -0700

    Moved the categories list to services.
    Created drop down menu for the product page.
    
    Search by categories is working.

--- client/app/sell/sell.js
+++ client/app/sell/sell.js
@@ -6,47 +6,7 @@ angular.module('shwop.sell', [])
   $scope.product.photoURL = '';
   $scope.product.category = null;
 
-  $scope.categories = [
-    {id: '1', name: 'Antiques'},
-    {id: '2', name: 'Appliances'},
-    {id: '3', name: 'Arts, Crafts & Sewing'},
-    {id: '4', name: 'Automotive'},
-    {id: '5', name: 'Baby & Kids'},
-    {id: '6', name: 'Barter'},
-    {id: '7', name: 'Beauty'},
-    {id: '8', name: 'Bikes'},
-    {id: '9', name: 'Boats'},
-    {id: '10', name: 'Books'},
-    {id: '11', name: 'Cell Phones & Accessories'},
-    {id: '12', name: 'Clothing, Shoes & Jewelry - Women'},
-    {id: '13', name: 'Clothing, Shoes & Jewelry - Men'},
-    {id: '14', name: 'Clothing, Shoes & Jewelry - Girls'},
-    {id: '15', name: 'Clothing, Shoes & Jewelry - Boys'},
-    {id: '16', name: 'Clothing, Shoes & Jewelry - Baby'},
-    {id: '17', name: 'Collectibles & Fine Art'},
-    {id: '18', name: 'Computers & Software'},
-    {id: '19', name: 'Electronics'},
-    {id: '20', name: 'Free'},
-    {id: '21', name: 'Furniture'},
-    {id: '22', name: 'Games & Toys'},
-    {id: '23', name: 'Garage Sale'},
-    {id: '24', name: 'General / Miscellaneous'},
-    {id: '25', name: 'Grocery & Food'},
-    {id: '26', name: 'Handmade'},
-    {id: '27', name: 'Health & Personal Care'},
-    {id: '28', name: 'Home & Kitchen'},
-    {id: '29', name: 'Industrial & Scientific'},
-    {id: '30', name: 'Luggage & Travel Gear'},
-    {id: '31', name: 'Movies & TV'},
-    {id: '32', name: 'Music & Musical Instruments'},
-    {id: '33', name: 'Office & Business'},
-    {id: '34', name: 'Patio, Lawn & Garden'},
-    {id: '35', name: 'Pet Supplies'},
-    {id: '36', name: 'Sports & Outdoors'},
-    {id: '37', name: 'Tickets'},
-    {id: '38', name: 'Tools & Home Improvement'},
-    {id: '39', name: 'Video Games'}
-  ];
+  $scope.categories = Products.categories
 
   // Helper function that adds an item to an array if the item is not already
   // in the array and the item is not blank. 
