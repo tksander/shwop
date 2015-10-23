e9947c9f314fe0173efc867a2e0cbd69f20ffcb0
Brett Sanders <bsand08@gmail.com>
2015-10-22 17:54:04 -0700

    (feat) check auth.

--- server/users/userController.js
+++ server/users/userController.js
@@ -25,33 +25,10 @@ module.exports = {
     .catch(function (error) {
       next(error);
     });
-    // var findUser = Q.nbind(User.findOne, User);
-    // findUser({username: username})
-    //   .then(function (user) {
-    //     if (!user) {
-    //       next(new Error('User does not exist'));
-    //     } else {
-    //       return user.comparePasswords(password)
-    //         .then(function (foundUser) {
-    //           if (foundUser) {
-    //             var token = jwt.encode(user, 'secret');
-    //             res.json({token: token});
-    //           } else {
-    //             return next(new Error('No user'));
-    //           }
-    //         });
-    //     }
-    //   })
-    //   .fail(function (error) {
-    //     next(error);
-    //   });
   },
 
   signup: function (req, res, next) {
-    // var create;
-    // var newUser;
 
-    // var findOne = Q.nbind(User.findOne, User);
     db.User.findOne({
       where: {email: req.body.email}
     })
@@ -75,30 +52,6 @@ module.exports = {
     .catch(function (error) {
       next(error);
     });
-
-    // // check to see if user already exists
-    // findOne({username: username})
-    //   .then(function (user) {
-    //     if (user) {
-    //       next(new Error('User already exist!'));
-    //     } else {
-    //       // make a new user if not one
-    //       create = Q.nbind(User.create, User);
-    //       newUser = {
-    //         username: username,
-    //         password: password
-    //       };
-    //       return create(newUser);
-    //     }
-    //   })
-    //   .then(function (user) {
-    //     // create token to send back for auth
-    //     var token = jwt.encode(user, 'secret');
-    //     res.json({token: token});
-    //   })
-    //   .fail(function (error) {
-    //     next(error);
-    //   });
   },
 
   checkAuth: function (req, res, next) {
@@ -106,23 +59,22 @@ module.exports = {
     // grab the token in the header is any
     // then decode the token, which we end up being the user object
     // check to see if that user exists in the database
-    // var token = req.headers['x-access-token'];
-    // if (!token) {
-    //   next(new Error('No token'));
-    // } else {
-    //   var user = jwt.decode(token, 'secret');
-    //   var findUser = Q.nbind(User.findOne, User);
-    //   findUser({username: user.username})
-    //     .then(function (foundUser) {
-    //       if (foundUser) {
-    //         res.send(200);
-    //       } else {
-    //         res.send(401);
-    //       }
-    //     })
-    //     .fail(function (error) {
-    //       next(error);
-    //     });
-    // }
+    var token = req.headers['x-access-token'];
+    if (!token) {
+      next(new Error('No token'));
+    } else {
+      var user = jwt.decode(token, 'secret');
+      db.User.findOne({where: {username: user.username}})
+      .then(function (foundUser) {
+        if (foundUser) {
+          res.send(200);
+        } else {
+          res.send(401);
+        }
+      })
+      .catch(function (error) {
+        next(error);
+      });
+    }
   }
 };
