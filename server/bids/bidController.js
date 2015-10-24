02826349a73a17cd4c6939e633d064a438b4dab1
Brett Sanders <bsand08@gmail.com>
2015-10-23 18:08:42 -0700

    (feat) wrap require for sneakyLocal in If statement

--- server/bids/bidController.js
+++ server/bids/bidController.js
@@ -1,6 +1,9 @@
 var db = require('../db/db_config.js');
 var util = require('../config/utils.js');
-var locally = require('../../sneakyLocal.js');
+var db = require('../db/db_config.js');
+if(!process.env.TwilioSid) {
+  var locally = require('../../sneakyLocal.js');
+}
 var client = require('twilio')(process.env.TwilioSid || locally.TwilioSid, process.env.TwilioAuthToken || locally.TwilioAuthToken);
 
 module.exports = {
