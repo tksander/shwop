35ede9d8cc668a8879b922e7170a780eb7eac5f5
Corey Roy <corey.j.roy@gmail.com>
2015-10-26 15:23:30 -0700

    (fix) Fix photo upload button on sell page

--- client/app/sell/sell.js
+++ client/app/sell/sell.js
@@ -87,7 +87,6 @@ angular.module('shwop.sell', [])
 .directive('ngEnter', function () {
   return function (scope, element, attrs) {
     element.bind('keydown keypress', function (event) {
-      console.log(event.which);
       if(event.which === 13) {
         scope.$apply(function (){
           scope.$eval(attrs.ngEnter);
