fac83cf88cfcd5d4c5da70fc3a5f52c190b1adf0
Corey Roy <corey.j.roy@gmail.com>
2015-10-27 19:38:55 -0700

    (i18n) Add l10n for signin page

--- client/langs/strings_en.js
+++ client/langs/strings_en.js
@@ -1,7 +1,9 @@
 var enUS = {
   "signupPrompt": "No Account? Sign Up!",
   "emailPrompt": "Email",
+  "emailPlaceholder": "Enter your email here",
   "passwordPrompt": "Password",
+  "passwordPlaceholder": "Enter your password here",
   "cancelButton": "Cancel",
   "submitButton": "Submit"
 };
