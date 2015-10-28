5d5cfe6862362d69e73d68ecaaa923e38b7a7694
Brett Sanders <bsand08@gmail.com>
2015-10-28 14:54:11 -0700

    (feat) add update/save/cancel to profile page

--- client/app/profile/profile.js
+++ client/app/profile/profile.js
@@ -2,7 +2,11 @@ angular.module('shwop.profile', [])
 
 
 .controller('ProfileController', ['$scope', '$window', '$translate', 'Users', 'Auth', function ($scope, $window, $translate, Users, Auth) {
-  $scope.data = {};
+  $scope.data = {
+    user: {}
+  };
+
+  $scope.updateMode = false;
 
   $scope.signout = function() {
     Auth.signout();
@@ -29,11 +33,28 @@ angular.module('shwop.profile', [])
     .then(function (translatedValue) {
       $scope.data.user = {};
       alert(translatedValue);
+      $("input[type!='submit']").prop('disabled', true);
+      $scope.updateMode = false;
       $scope.getUserInfo();
     })
     .catch(function (err) {
       console.log('/api/users/update POST failed', err);
     });
+  };
+
+  $scope.cancelChanges = function () {
+    $("input[type!='submit']").prop('disabled', true);
+    $scope.updateMode = false;
+    $scope.getUserInfo();
+  };
+
+  $scope.enableUpdateMode = function () {
+    $('input').prop('disabled', false);
+    $scope.updateMode = true;
+  };
+
+  $scope.backToShwopping = function () {
+    $location.path( "/products" );
   };
 
   $scope.getUserInfo();
