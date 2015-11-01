5c53393fda99ac60a138ed94246b0506d8c5a7a0
tksander <tkepasander@gmail.com>
2015-10-31 17:31:05 -0700

    Added user auth protection to user routes

--- server/users/userRoutes.js
+++ server/users/userRoutes.js
@@ -4,10 +4,10 @@ var userController = require('../users/userController.js');
 module.exports = function (app) {
   // app === userRouter injected from middlware.js
 
-  app.post('/signin', userController.signin);
-  app.post('/signup', userController.signup);
+  app.post('/signin', userController.checkAuth, userController.signin);
+  app.post('/signup', userController.checkAuth, userController.signup);
   app.get('/signedin', userController.checkAuth);
-  app.post('/profile', userController.userInfo);
-  app.post('/update', userController.updateUser);
-  app.get('/:userId', userController.getUserLocation)
+  app.post('/profile', userController.checkAuth, userController.userInfo);
+  app.post('/update', userController.checkAuth, userController.updateUser);
+  app.get('/:userId', userController.checkAuth, userController.getUserLocation)
 };
