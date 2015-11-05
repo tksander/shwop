54cca523d89cc000258f20770fa74c4bc6b04b57
Corey Roy <corey.j.roy@gmail.com>
2015-11-04 20:25:20 -0800

    (feat) Added unit tests for bid.js and cleaned up sellUnitTests.js

--- test/client/unit_testing/controllers/sellUnitTests.js
+++ test/client/unit_testing/controllers/sellUnitTests.js
@@ -1,38 +1,19 @@
 
 describe('SellController', function() {
-  var backend, $rootScope, $location, $window, createController, authRequestHandler;
+  var createController;
   var $controller;
 
   var exampleArray = [1, 3, 5, 7];
 
   beforeEach(module('shwop'));
 
-  // define(['angular', 'angular-translate'], function (angular) {
-  //     angular.module('shwop.sell', ['pascalprecht.translate']);
-  // });
-
-  beforeEach(inject(function(_$controller_) {
-      // var scope = $rootScope.$new();
-      // var translate = $translate;
+  beforeEach(inject(function(_$controller_, Auth) {
       $controller = _$controller_;
-      // var testCont = $controller('SellController', {$scope: scope});
+      AuthService = Auth;
+      sinon.spy(AuthService,'signout');
   }));
 
-  // beforeEach(inject(function($injector) {
-  //   backend = $injector.get('$httpBackend');
-  //   $rootScope = $injector.get('$rootScope');
-
-  //   createController = function() {
-  //     return $controller('SellController', {'$scope': $rootScope});
-  //   };
-  // }));
-
-  // afterEach(function() {
-  //   backend.verifyNoOutstandingExpectation();
-  //   backend.verifyNoOutstandingRequest();
-  // });
-
-  describe('addTag', function() {
+  describe('$scope.addTag', function() {
     it('Should add the passed-in tag to the passed-in array', function(){
       var $scope = {};
       $scope.data = {};
@@ -45,23 +26,13 @@ describe('SellController', function() {
     });
   });
 
-  // describe('addToArray', function() {
-  //   it('Should add an item to the specified array if it is not already present', function(){
-  //     $scope.addToArray(2, exampleArray);
-  //     $scope.addToArray(4, exampleArray);
-  //     $scope.addToArray(2, exampleArray);
-  //     $scope.addToArray('', exampleArray);
-  //     expect(exampleArray).to.equal([1, 3, 5, 7, 2, 4]);
-  //   });
-  // });
-
-  // describe('isBlank', function() {
-  //   it('Should return "true" if the passed-in item is an empty string and otherwise return "false"', function(){
-  //     var $scope = {};
-  //     var controller = $controller('SellController', {$scope: $scope});
-  //     expect(isBlank('')).to.equal(true);
-  //     expect(isBlank(' ')).to.equal(false);
-  //     expect('So long and thanks for all the fish!').to.equal(false);
-  //   });
-  // });
+  describe('$scope.signout', function() {
+    it('calls the signout method on the Auth service', function() {
+      var $scope = {};
+      var controller = $controller('SellController', { $scope: $scope });
+      $scope.signout();
+      assert(AuthService.signout.calledOnce);
+    });
+  });
 });
+
